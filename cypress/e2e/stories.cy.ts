5083ad50115e206a691608b4e0fdd02aff0e148e
sagars0600 <sagarsolanki2120@gmail.com>
2025-06-02 17:49:01 +0530

    test cases

--- cypress/e2e/stories.cy.ts
+++ cypress/e2e/stories.cy.ts
@@ -1,13 +1,16 @@
 /// <reference types="cypress" />
-describe('Instagram Stories Feature', () => {
+describe('Instagram Stories Feature (Mobile)', () => {
   beforeEach(() => {
+    // Set mobile viewport before visiting the page
+    cy.viewport('iphone-x'); // or any other mobile device like 'iphone-6', 'samsung-s10', etc.
+
     // Visit the home page
     cy.visit('/');
   });
 
   it('should display a list of stories', () => {
-    // Check if the stories are loaded
+    // Check if the stories are loaded on mobile
     cy.contains('Instagram Stories').should('be.visible');
     cy.get('[class*="StoryThumbnail"]').should('have.length.at.least', 1);
   });
-})
+});
